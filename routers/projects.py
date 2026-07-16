fe2fc53cb2a3e9dc79d29340579edf641b71779e
Deepak Sahu <dsahu9555@gmail.com>
2026-07-17 00:37:35 +0530

    Build Project Settings

--- routers/projects.py
+++ routers/projects.py
@@ -7,6 +7,20 @@ router = APIRouter(
     tags=["projects"]
 )
 
+class ProjectSettings(BaseModel):
+    project_id: str
+    embedding_model: str
+    rag_strategy: str
+    agent_type: str
+    chunks_per_search: int
+    final_context_size: int
+    similarity_threshold: float
+    number_of_queries: int
+    reranking_enabled: bool
+    reranking_model: str
+    vector_weight: float
+    keyword_weight: float
+
 class ProjectCreate(BaseModel):
     name: str
     description: str
@@ -148,3 +162,29 @@ def get_project_settings(
         }     
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to get project settings: {str(e)}")
+    
+
+@router.put("/api/projects/{project_id}/settings")
+def update_project_settings(
+    project_id: str,
+    settings: ProjectSettings,
+    clerk_id: str = Depends(get_current_user) 
+):
+    try:
+        # always verify
+        result = supabase.table("projects").select("id").eq("id", project_id).eq("clerk_id", clerk_id).execute()
+        
+        if not result.data:
+            raise HTTPException(status_code=404, detail="Project settings not found or access denied")
+        
+        updated_settings = supabase.table("project_settings").update(settings.model_dump()).eq("project_id", project_id).execute()
+        
+        if not updated_settings.data:
+            raise HTTPException(status_code=404, detail=f"Project settings not found")
+        
+        return{
+            "message": "Project settings updated successfully!",
+            "data": updated_settings.data[0]
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to update project settings: {str(e)}")
